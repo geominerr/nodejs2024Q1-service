89f7fdcbfcecbf877173dd32a52da41fb648cb91
Maksym Voloshyn <mv.webdew@gmail.com>
2024-03-09 11:18:19 +0200

    feat: add albums module

---
+++ src/albums/albums.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  HttpCode,
+  ParseUUIDPipe,
+  Put,
+} from '@nestjs/common';
+import { AlbumsService } from './albums.service';
+import { CreateAlbumDto } from './dto/create-album.dto';
+import { UpdateAlbumDto } from './dto/update-album.dto';
+
+@Controller('album')
+export class AlbumsController {
+  constructor(private readonly albumsService: AlbumsService) {}
+
+  @Post()
+  create(@Body() createAlbumDto: CreateAlbumDto) {
+    return this.albumsService.create(createAlbumDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.albumsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.albumsService.findOne(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateAlbumDto: UpdateAlbumDto,
+  ) {
+    return this.albumsService.update(id, updateAlbumDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.albumsService.remove(id);
+  }
+}
---
+++ src/albums/dto/create-album.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, IsNumber, IsNotEmpty } from 'class-validator';
+
+export class CreateAlbumDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  year: number;
+
+  artistId: string | null;
+}
