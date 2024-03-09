9de75275206c17ce2f8a0a8b1dc3444e6ddc1a86
Maksym Voloshyn <mv.webdew@gmail.com>
2024-03-09 13:24:11 +0200

    feat: add config

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
+  const port = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(4000);
+  await app.listen(port);
 }
+
 bootstrap();
